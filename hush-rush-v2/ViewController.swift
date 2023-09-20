49f320b010b22b73e615727397d0140e3e687163
Sherary <sherary.apriliana@gmail.com>
2023-09-20 19:35:23 +0700

    Initial commit

---
+++ hush-rush-v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hush-rush-v2
+//
+//  Created by Sherary Apriliana on 20/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
